4c609d6066d7c2dfb6461521c3486eddaff99bcd
jungsikyeo <mdotdom12@naver.com>
2021-03-07 06:13:54 +0900

    project init

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './styles/App.css';
+import './styles/codicon.css'
+import './styles/tailwind.css'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p className="codicon codicon-add text-red-500">
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
