5511b7ad67af64468a6f0046ae77a679c9fe95d9
jungsikyeo <mdotdom12@naver.com>
2021-03-16 07:58:32 +0900

    tab 기능 수정중

--- src/pages/main-right.tsx
+++ src/pages/main-right.tsx
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
+import Tabs, { Tab } from "./tabs";
 import "../styles/theme.css";
+import "../styles/react-tabs.scss";
 
 export const MainRight = () => {
+  const [activeTab, handleTabSwitch] = useState(0);
+  const tabs: any[] = [];
+  const handleTabPositionChange = (a: number, b: number) => {
+    let c = tabs[a];
+    tabs[a] = tabs[b];
+    tabs[b] = c;
+
+    if (activeTab === a) {
+      handleTabSwitch(b);
+    } else if (activeTab === b) {
+      handleTabSwitch(a);
+    }
+  };
   return (
     <div
       className="h-full absolute whitespace-normal"
@@ -12,7 +27,16 @@ export const MainRight = () => {
         zIndex: 100,
       }}
     >
-      right
+      <Tabs
+        active={activeTab}
+        onTabSwitch={handleTabSwitch}
+        onTabPositionChange={handleTabPositionChange}
+        draggable={true}
+      >
+        <Tab key="0" title="tab1">foo</Tab>
+        <Tab key="1" title="tab2">bar</Tab>
+        <Tab key="2" title="tab3">baz</Tab>
+      </Tabs>
     </div>
   );
 };
